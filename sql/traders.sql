83fefa87b11f462f9b516da5f09d1568a42dbca8
CoftSock <c0rp3rman1c@gmail.com>
2016-02-18 13:00:04 +1100

    Update traders.sql

--- sql/traders.sql
+++ sql/traders.sql
@@ -1,11 +1,37 @@
-CREATE TABLE IF NOT EXISTS `buy_history` (
-  `pUID` varchar(64) NOT NULL,
+CREATE TABLE IF NOT EXISTS `car_buy_history` (
+  `pUID` varchar(32) NOT NULL,
   `price` varchar(64) NOT NULL,
-  `item` varchar(64) NOT NULL
+  `item` varchar(64) NOT NULL,
+  `ppos` varchar(64) NOT NULL,
+  `player_money` varchar(64) NOT NULL,
+  `date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `mod_buy_history` (
+  `pUID` varchar(32) NOT NULL,
+  `price` varchar(64) NOT NULL,
+  `item` varchar(64) NOT NULL,
+  `ppos` varchar(64) NOT NULL,
+  `player_money` varchar(64) NOT NULL,
+  `skin` varchar(64) NOT NULL,
+  `date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `sell_history` (
-  `pUID` varchar(64) NOT NULL,
+  `pUID` varchar(32) NOT NULL,
   `price` varchar(64) NOT NULL,
-  `item` varchar(64) NOT NULL
+  `item` varchar(64) NOT NULL,
+  `ppos` varchar(64) NOT NULL,
+  `quantity` varchar(64) NOT NULL,
+  `player_money` varchar(64) NOT NULL,
+  `date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `waste_history` (
+  `pUID` varchar(32) NOT NULL,
+  `price` varchar(64) NOT NULL,
+  `item` varchar(64) NOT NULL,
+  `ppos` varchar(64) NOT NULL,
+  `player_money` varchar(64) NOT NULL,
+  `date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
